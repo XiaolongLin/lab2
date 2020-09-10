f263d0759f1b215bf0787597fbb96b6bec8f6dd7
Xiaolong Lin <xxl5453@psu.edu>
2020-09-10 19:40:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+# Author: Xiaolong Lin xxl5453@psu.edu
+# Collaborator:Manraj Kumar mmk6281@psu.edu
+# Collaborator:John Malay jmm9098@psu.edu   
+# Collaborator:Irfan Khan imk5134@psu.edu
+# Section: 006R
+# Breakout: 15
+
+def getLettergrade(grade):
+  return "A"
+def run():
+  ignore = input("Anything: ")
+  print(getLettergrade(100.0))
+
+if __name__ ==  "__main__":
+  run()
